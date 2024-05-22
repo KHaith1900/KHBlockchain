ceb98b077fc4a6ad0ac6fd541074a3d60355f05c
KHaith1900 <kendrickhaith@gmail.com>
2024-05-22 12:22:44 -0400

    created new class Block and migrated code from chain to Block also created StringUtil class which creates the hash to be used in Blocks

--- chain.java
+++ chain.java
@@ -1,21 +1,9 @@
-import java.util.Date;
 
-public class chain{
-    public String hash;
-    public String prevHash;
-    private String data;
-    private long timeStamp;
 
-    //Block Constructor
-    public chain(String data, String prevHash)
-    {
-        this.data = data;
-        this.prevHash = prevHash;
-        this.timeStamp = new Date().getTime();
-    }
+public class chain
+{
     public static void main(String[] args)
     {
         
     }
 }
-
